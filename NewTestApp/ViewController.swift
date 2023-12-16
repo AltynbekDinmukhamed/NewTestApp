3825d122b5ff782841b0d6fa4174a0fac4f83cf8
AltynbekDinmukhamed <a.dimash.a_03@mail.ru>
2023-12-16 15:54:48 +0600

    Initial Commit

---
+++ NewTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTestApp
+//
+//  Created by Димаш Алтынбек on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
